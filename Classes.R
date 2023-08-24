47a1d566e8c83ec89ee2bc6e9076818c0b8f5816
Rodrigo Aluizio <r.aluizio@gmail.com>
2023-08-24 10:59:28 -0300

    restructured project tree

--- Classes.R
+++
@@ -1,4 +0,0 @@
-#Formalizaçao das classes referentes as funçoes do pacote forams
-setClass("abc", representation(abc = "data.frame", W.Stat = "numeric"))
-setClass("she", representation(bi = "data.frame"))
-setClass("fi", representation(fi = "data.frame"))
--- README.md
+++
@@ -1,6 +0,0 @@
-forams
-======
-
-GIT repository for the forams R package.
-
-Detais: https://cran.r-project.org/package=forams
